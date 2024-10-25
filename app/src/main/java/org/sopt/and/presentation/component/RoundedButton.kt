e92b521aa98b4e5827bf914f9167b021a5d7c112
ësungmin <arcn1345@gmail.com>
2024-10-25 19:04:30 +0900

    [mod] : #3 RoundedButton 기본 배경색 설정

--- app/src/main/java/org/sopt/and/presentation/component/RoundedButton.kt
+++ app/src/main/java/org/sopt/and/presentation/component/RoundedButton.kt
@@ -1,11 +1,10 @@
 package org.sopt.and.presentation.component
 
-import android.graphics.Color
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -25,17 +24,17 @@ fun RoundedButton(
         onClick  = onClick,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(start = 16.dp, end = 16.dp)
-            .background(
-                color = WavveBlue,
-                shape = RoundedCornerShape(30.dp)
-                )
+            .padding(horizontal = 16.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = WavveBlue),
+        shape = RoundedCornerShape(30.dp)
 
     ) {
         Text(
             text = content,
-            style = TextStyle(fontSize = 20.sp),
-            color = White
+            style = TextStyle(fontSize = 16.sp),
+            color = White,
+            modifier = Modifier
+                .padding(vertical = 8.dp)
         )
     }
 }
