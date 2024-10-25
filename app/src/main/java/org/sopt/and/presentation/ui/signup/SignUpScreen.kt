4b5e9336711627575d84bb6754adc2522016fdfd
ësungmin <arcn1345@gmail.com>
2024-10-25 21:48:45 +0900

    [feat] : #3 SignUpScreen 중복 파일 제거 및 생성

--- app/src/main/java/org/sopt/and/presentation/ui/signup/SignUpScreen.kt
+++ app/src/main/java/org/sopt/and/presentation/ui/signup/SignUpScreen.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +14,7 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material3.Icon
@@ -48,128 +48,142 @@ fun SignUpScreen(
     isSignUp: (String, String) -> Unit,
     signUpSuccess: Boolean
 ) {
-
-    Column(
-    modifier = Modifier
-    .fillMaxSize()
-    .background(Black)
-    .statusBarsPadding()
-    .navigationBarsPadding()
-    .imePadding(),
-    horizontalAlignment = Alignment.CenterHorizontally
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Black)
+            .statusBarsPadding()
+            .navigationBarsPadding()
+            .imePadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Toolbar(
-            content = stringResource(R.string.signup),
-            leadingIcon = {
-                IconButton(onClick = navigateUp) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
-                        contentDescription = null,
-                        tint = White,
-                        modifier = Modifier
-                            .size(40.dp)
-                    )
+        item {
+            Toolbar(
+                content = stringResource(R.string.signup),
+                leadingIcon = {
+                    IconButton(onClick = navigateUp) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                            contentDescription = null,
+                            tint = White,
+                            modifier = Modifier
+                                .size(40.dp)
+                        )
+                    }
                 }
-            }
-        )
-
-        Text(
-            color = White,
-            text = stringResource(R.string.signup_title),
-            fontSize = 25.sp,
-            lineHeight = 32.sp,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 16.dp, end = 16.dp, top = 20.dp)
-        )
-
-        // Email 입력
-        SignTextField(
-            value = signUpEmail,
-            onValueChange = onEmailChange,
-            isPwdVisible = true,
-            placeholder = stringResource(R.string.signup_id_placeholder),
-            modifier = Modifier.padding(10.dp)
-        )
-        Text(
-            color = White,
-            text = stringResource(R.string.signup_id_warning),
-            fontSize = 14.sp,
-            modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp)
-        )
-
-        // Password 입력
-        SignTextField(
-            value = signUpPwd,
-            onValueChange = onPwdChange,
-            placeholder = stringResource(R.string.signup_pwd_placeholder),
-            modifier = Modifier.padding(10.dp),
-            isPwdVisible = isPwdVisibility,
-            onPwdVisibilityChange = isPwdVisible
-        )
-        Text(
-            color = White,
-            text = stringResource(R.string.signup_pwd_warning),
-            fontSize = 14.sp,
-            modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp)
-        )
-        Spacer(modifier = Modifier.height(20.dp))
-
-        Text(
-            color = White,
-            text = stringResource(R.string.sign_social),
-            fontSize = 14.sp
-        )
-        Row (
-            modifier = Modifier
-                .padding(top = 20.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            Image(
-                painter = painterResource(R.drawable.btn_kakao),
-                contentDescription = "Facebook",
-                modifier = Modifier
-                    .size(32.dp)
-
-            )
-            Image(
-                painter = painterResource(R.drawable.btn_naver),
-                contentDescription = "Twitter",
-                modifier = Modifier
-                    .size(32.dp)
-            )
-            Image(
-                painter = painterResource(R.drawable.btn_google),
-                contentDescription = "Google",
-                modifier = Modifier
-                    .size(32.dp)
             )
         }
-        Spacer(modifier = Modifier.height(40.dp))
 
-        Text(
-            color = White,
-            text = stringResource(R.string.warning),
-            fontSize = 14.sp,
-            modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp)
-        )
+        item {
+            Text(
+                color = White,
+                text = stringResource(R.string.signup_title),
+                fontSize = 25.sp,
+                lineHeight = 32.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp, top = 20.dp)
+            )
+        }
 
-        Spacer(modifier = Modifier.weight(1f))
+        item {
+            // Email 입력
+            SignTextField(
+                value = signUpEmail,
+                onValueChange = onEmailChange,
+                isPwdVisible = true,
+                placeholder = stringResource(R.string.signup_id_placeholder),
+                modifier = Modifier.padding(10.dp)
+            )
+            Text(
+                color = White,
+                text = stringResource(R.string.signup_id_warning),
+                fontSize = 14.sp,
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp)
+            )
+        }
+
+        item {
+            // Password 입력
+            SignTextField(
+                value = signUpPwd,
+                onValueChange = onPwdChange,
+                placeholder = stringResource(R.string.signup_pwd_placeholder),
+                modifier = Modifier.padding(10.dp),
+                isPwdVisible = isPwdVisibility,
+                onPwdVisibilityChange = isPwdVisible
+            )
+            Text(
+                color = White,
+                text = stringResource(R.string.signup_pwd_warning),
+                fontSize = 14.sp,
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp)
+            )
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                color = White,
+                text = stringResource(R.string.sign_social),
+                fontSize = 14.sp
+            )
+            Row(
+                modifier = Modifier
+                    .padding(top = 20.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.btn_kakao),
+                    contentDescription = "kakao",
+                    modifier = Modifier
+                        .size(32.dp)
+
+                )
+                Image(
+                    painter = painterResource(R.drawable.btn_naver),
+                    contentDescription = "naver",
+                    modifier = Modifier
+                        .size(32.dp)
+                )
+                Image(
+                    painter = painterResource(R.drawable.btn_google),
+                    contentDescription = "Google",
+                    modifier = Modifier
+                        .size(32.dp)
+                )
+            }
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(40.dp))
+            Text(
+                color = White,
+                text = stringResource(R.string.warning),
+                fontSize = 14.sp,
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp)
+            )
+        }
 
         // 회원가입 버튼
-        RoundedButton(
-            content = stringResource(R.string.signup),
-            onClick = {
-                isSignUp(signUpEmail, signUpPwd)
-            }
-        )
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
+            RoundedButton(
+                content = stringResource(R.string.signup),
+                onClick = {
+                    isSignUp(signUpEmail, signUpPwd)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
+        }
     }
 
     if (signUpSuccess) {
         LaunchedEffect(Unit) {
             navigateSignIn() // 회원가입 성공 시 navigateSignIn 호출
-            Log.d("로그","LaunchedEffect navigateSignIn")
+            Log.d("로그", "LaunchedEffect navigateSignIn")
         }
     }
-
 }
