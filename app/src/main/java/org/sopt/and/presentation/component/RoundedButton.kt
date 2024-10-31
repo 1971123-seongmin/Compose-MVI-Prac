2328529fabcc5721f5e3f6c9767fc90774962238
ësungmin <arcn1345@gmail.com>
2024-10-31 20:26:40 +0900

    [chore] : #6 RoundedButton style 수정

--- app/src/main/java/org/sopt/and/presentation/component/RoundedButton.kt
+++ app/src/main/java/org/sopt/and/presentation/component/RoundedButton.kt
@@ -1,6 +1,7 @@
 package org.sopt.and.presentation.component
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -9,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.sopt.and.ui.theme.WavveBlue
@@ -18,23 +20,22 @@ import org.sopt.and.ui.theme.White
 fun RoundedButton(
     content: String,
     onClick: () -> Unit,
-    modifier: Modifier = Modifier
 ) {
     Button(
         onClick  = onClick,
         modifier = Modifier
             .fillMaxWidth()
+            .height(48.dp)
             .padding(horizontal = 16.dp),
         colors = ButtonDefaults.buttonColors(containerColor = WavveBlue),
-        shape = RoundedCornerShape(30.dp)
+        shape = RoundedCornerShape(32.dp)
 
     ) {
         Text(
             text = content,
             style = TextStyle(fontSize = 16.sp),
             color = White,
-            modifier = Modifier
-                .padding(vertical = 8.dp)
+            textAlign = TextAlign.Center
         )
     }
 }
