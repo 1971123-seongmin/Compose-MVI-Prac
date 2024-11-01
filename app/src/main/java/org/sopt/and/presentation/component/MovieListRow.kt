3dfb8f815c662bd621d2c017ec25534da39caf60
ësungmin <arcn1345@gmail.com>
2024-11-01 19:42:13 +0900

    [mod] : #6 MovieListRow 불변 리스트로 변경

--- app/src/main/java/org/sopt/and/presentation/component/MovieListRow.kt
+++ app/src/main/java/org/sopt/and/presentation/component/MovieListRow.kt
@@ -1,13 +1,14 @@
 package org.sopt.and.presentation.component
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,11 +16,12 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import kotlinx.collections.immutable.ImmutableList
 
 // 홈화면 영화 LazyRow
 @Composable
 fun MovieListRow(
-    movieList: List<Int>, // 영화 이미지의 리소스 ID 리스트
+    @DrawableRes movieList: ImmutableList<Int>,
     modifier: Modifier = Modifier
 ) {
     LazyRow (
@@ -29,9 +31,9 @@ fun MovieListRow(
             .padding(horizontal = 8.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        items(movieList.size) { index ->
+        items(movieList) { movie ->
             Image(
-                painter = painterResource(movieList[index]),
+                painter = painterResource(movieList[movie]),
                 contentDescription = "배너",
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
