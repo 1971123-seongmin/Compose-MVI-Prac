2ddf6de4158119f5f40627d6b11528c6a773533f
ësungmin <arcn1345@gmail.com>
2024-11-06 19:28:24 +0900

    [feat] : #8 BaseResponse 추가

---
+++ app/src/main/java/org/sopt/and/base/BaseResponse.kt
@@ -0,0 +1,20 @@
+package org.sopt.and.base
+
+import kotlinx.serialization.SerialName
+
+data class BaseResponse<T>(
+    @SerialName("code")
+    val code: String,
+    @SerialName("message")
+    val message: String,
+    @SerialName("result")
+    val result: T,
+)
+
+data class NullableBaseResponse<T>(
+    @SerialName("code")
+    val code: String? = null,
+    @SerialName("message")
+    val message: String? = null,
+    val result: T,
+)
