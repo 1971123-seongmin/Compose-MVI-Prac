798734f7137a602dcf9ff62f8f950e942d402939
ësungmin <arcn1345@gmail.com>
2024-11-06 18:46:30 +0900

    [feat] : #8 InfoDataStore -> TokenManger 이름 변경 및 토큰 추가

--- app/src/main/java/org/sopt/and/utils/TokenManger.kt
+++ app/src/main/java/org/sopt/and/utils/TokenManger.kt
@@ -6,15 +6,34 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
 
-class InfoDataStore(private val context: Context) {
+class TokenManger @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
 
     companion object {
         private val Context.infoDataStore: DataStore<Preferences> by preferencesDataStore(name = "info")
+        private val ACCESS_TOKEN_KEY = stringPreferencesKey("access_token")
         val EMAIL = stringPreferencesKey("user_email")
         val PWD = stringPreferencesKey("user_pwd")
+    }
+
+    fun saveToken(token: String) = runBlocking {
+        context.infoDataStore.edit { prefs ->
+            prefs[ACCESS_TOKEN_KEY] = token
+        }
+    }
+
+    fun getAccessToken(): Flow<String?> {
+        return  context.infoDataStore.data.map { prefs ->
+            prefs[ACCESS_TOKEN_KEY]
+        }
     }
 
     suspend fun saveEmail(email: String) {
