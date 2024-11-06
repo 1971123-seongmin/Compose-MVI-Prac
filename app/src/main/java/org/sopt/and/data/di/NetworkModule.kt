e7674da5c34ce260615b3ed9da21320327e41e1b
ësungmin <arcn1345@gmail.com>
2024-11-06 18:46:40 +0900

    [feat] : #8 NetworkModule 추가

---
+++ app/src/main/java/org/sopt/and/data/di/NetworkModule.kt
@@ -0,0 +1,86 @@
+package org.sopt.and.data.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.sopt.and.BuildConfig
+import org.sopt.and.data.interceptor.AccessTokenInterceptor
+import org.sopt.and.utils.TokenManger
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class MainServer
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class Token
+
+    @Provides
+    @Singleton
+    @OptIn(ExperimentalSerializationApi::class)
+    fun provideJson(): Json = Json {
+        isLenient = true
+        prettyPrint = true
+        explicitNulls = false
+        ignoreUnknownKeys = true
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    @Provides
+    @Singleton
+    @Token
+    fun provideTokenInterceptor(tokenManager: TokenManger): AccessTokenInterceptor {
+        return AccessTokenInterceptor(tokenManager)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOKHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+        accessTokenInterceptor: AccessTokenInterceptor
+    ): OkHttpClient =
+        OkHttpClient.Builder().apply {
+            connectTimeout(30, TimeUnit.SECONDS)
+            readTimeout(30, TimeUnit.SECONDS)
+            writeTimeout(30, TimeUnit.SECONDS)
+            addInterceptor(httpLoggingInterceptor)
+            addInterceptor(accessTokenInterceptor)
+        }.build()
+
+    @ExperimentalSerializationApi
+    @MainServer
+    @Provides
+    @Singleton
+    fun provideMainRetrofit(
+        okHttpClient: OkHttpClient, json: Json
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_SERVER_URL)
+            .client(okHttpClient)
+            .addConverterFactory(json.asConverterFactory((requireNotNull("application/json".toMediaTypeOrNull()))))
+            .build()
+    }
+
+}
