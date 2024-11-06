30aadd5e2db339c4d6543f4a33e78ce4469e9a9f
ësungmin <arcn1345@gmail.com>
2024-11-06 17:46:03 +0900

    [feat] : #8 SocialLoginRow list remember 추가

--- app/src/main/java/org/sopt/and/presentation/component/SocialLoginRow.kt
+++ app/src/main/java/org/sopt/and/presentation/component/SocialLoginRow.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -17,13 +18,15 @@ import org.sopt.and.R
 
 @Composable
 fun SocialLoginRow() {
-    val socialButtons = listOf(
-        Pair(R.drawable.btn_kakao, R.string.content_desc_kakao),
-        Pair(R.drawable.btn_t_world, R.string.content_desc_t_world),
-        Pair(R.drawable.btn_naver, R.string.content_desc_naver),
-        Pair(R.drawable.btn_facebook, R.string.content_desc_facebook),
-        Pair(R.drawable.btn_apple, R.string.content_desc_apple)
-    )
+    val socialButtons = remember {
+        listOf(
+            Pair(R.drawable.btn_kakao, R.string.content_desc_kakao),
+            Pair(R.drawable.btn_t_world, R.string.content_desc_t_world),
+            Pair(R.drawable.btn_naver, R.string.content_desc_naver),
+            Pair(R.drawable.btn_facebook, R.string.content_desc_facebook),
+            Pair(R.drawable.btn_apple, R.string.content_desc_apple)
+        )
+    }
 
     Row(
         horizontalArrangement = Arrangement.spacedBy(16.dp)
