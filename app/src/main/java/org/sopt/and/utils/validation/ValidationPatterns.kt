90d33214ba59ca6aa138fa870b46572d1bdfa6e2
ësungmin <arcn1345@gmail.com>
2024-12-06 16:17:52 +0900

    [chore] : #10 미사용 코드, 파일 제거

--- app/src/main/java/org/sopt/and/utils/validation/ValidationPatterns.kt
+++
@@ -1,8 +0,0 @@
-package org.sopt.and.utils.validation
-
-import android.util.Patterns
-import java.util.regex.Pattern
-
-object ValidationPatterns {
-    val EMAIL: Pattern = Patterns.EMAIL_ADDRESS
-}
