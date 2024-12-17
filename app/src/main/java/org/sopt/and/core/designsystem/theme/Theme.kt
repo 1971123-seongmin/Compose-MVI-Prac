73644da73c2ae086465977433f1ec96b48bfa7ca
ësungmin <arcn1345@gmail.com>
2024-12-17 16:05:57 +0900

    [feat] : #12 statusBarColor 설정

--- app/src/main/java/org/sopt/and/core/designsystem/theme/Theme.kt
+++ app/src/main/java/org/sopt/and/core/designsystem/theme/Theme.kt
@@ -9,7 +9,12 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -36,10 +41,13 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun ANDANDROIDTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    backgroundColor: Color = Black,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+    val view = LocalView.current
+
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
@@ -48,6 +56,15 @@ fun ANDANDROIDTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.run {
+                statusBarColor = backgroundColor.toArgb()
+                WindowCompat.getInsetsController(this, view).isAppearanceLightStatusBars = true
+            }
+        }
     }
 
     MaterialTheme(
