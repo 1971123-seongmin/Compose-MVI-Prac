732ec90b718b6f65503f40af416a79ee7b5dd6f1
seognmin <arcn1345@gmail.com>
2025-01-08 19:12:32 +0900

    [feat/#5] GoogleAuthModule 코드 수정 (주석)

--- app/src/main/java/org/sopt/and/di/GoogleAuthModule.kt
+++ app/src/main/java/org/sopt/and/di/GoogleAuthModule.kt
@@ -21,21 +21,21 @@ object GoogleAuthModule {
     @Singleton
     fun provideGoogleIdOptions(): GetGoogleIdOption {
         return GetGoogleIdOption.Builder()
-            .setFilterByAuthorizedAccounts(true)
-            .setAutoSelectEnabled(true) // 재방문 사용자의 자동 로그인 사용 설정
+            .setFilterByAuthorizedAccounts(false)
+            .setAutoSelectEnabled(false) // 재방문 사용자의 자동 로그인 사용 설정
             .setServerClientId(BuildConfig.GOOGLE_CLIENT_ID) // 클라이언트 ID 설정
             .build()
     }
 
     // 신규 사용자 가입에 필요한 GetGoogleIdOption 객체를 제공
-    @Provides
-    @Singleton
-    fun provideGoogleSignUpOption(): GetGoogleIdOption {
-        return GetGoogleIdOption.Builder()
-            .setFilterByAuthorizedAccounts(false) // 기존 계정 필터링 해제 (신규 유저)
-            .setServerClientId(BuildConfig.GOOGLE_CLIENT_ID)
-            .build()
-    }
+//    @Provides
+//    @Singleton
+//    fun provideGoogleSignUpOption(): GetGoogleIdOption {
+//        return GetGoogleIdOption.Builder()
+//            .setFilterByAuthorizedAccounts(false) // 기존 계정 필터링 해제 (신규 유저)
+//            .setServerClientId(BuildConfig.GOOGLE_CLIENT_ID)
+//            .build()
+//    }
 
     // Google의 최신 인증 API를 사용하는 객체
     @Provides
