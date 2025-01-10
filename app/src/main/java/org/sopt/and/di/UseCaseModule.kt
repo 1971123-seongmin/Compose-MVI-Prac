809f58f6cb81b4a64cb050732aedb124ded0880c
seognmin <arcn1345@gmail.com>
2025-01-10 18:16:32 +0900

    [del/#7] DataSourceModule 삭제

--- app/src/main/java/org/sopt/and/di/UseCaseModule.kt
+++ app/src/main/java/org/sopt/and/di/UseCaseModule.kt
@@ -5,12 +5,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import org.sopt.and.domain.repository.AuthRepository
-import org.sopt.and.domain.repository.UserRepository
-import org.sopt.and.domain.repository.LocalHomeImageRepository
-import org.sopt.and.domain.usecase.auth.LoginUserUseCase
-import org.sopt.and.domain.usecase.auth.RegisterUserUseCase
-import org.sopt.and.domain.usecase.local.GetLocalHomeImageUseCase
-import org.sopt.and.domain.usecase.user.GetMyHobbyUseCase
+import org.sopt.and.domain.usecase.LoginUserUseCase
+import org.sopt.and.domain.usecase.RegisterUserUseCase
 import javax.inject.Singleton
 
 @Module
@@ -25,15 +21,5 @@ object UseCaseModule {
     @Singleton
     fun provideLoginUseCase(authRepository: AuthRepository): LoginUserUseCase =
         LoginUserUseCase(authRepository)
-
-    @Provides
-    @Singleton
-    fun provideGetMyHobbyUseCase(userRepository: UserRepository): GetMyHobbyUseCase =
-        GetMyHobbyUseCase(userRepository)
-
-    @Provides
-    @Singleton
-    fun provideGetLocalHomeImageUseCase(localHomeImageRepository: LocalHomeImageRepository):
-            GetLocalHomeImageUseCase = GetLocalHomeImageUseCase(localHomeImageRepository)
 
 }
