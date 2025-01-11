6465bd5ec1247a685b242e268207fbf1e46aa9c4
seognmin <arcn1345@gmail.com>
2025-01-12 03:48:28 +0900

    [mod/#9] UpdateUserRefreshTokenUseCase 반환형 domain layer의 모델로 변경

--- app/src/main/java/org/sopt/and/domain/usecase/UpdateUserRefreshTokenUseCase.kt
+++ app/src/main/java/org/sopt/and/domain/usecase/UpdateUserRefreshTokenUseCase.kt
@@ -1,13 +1,13 @@
 package org.sopt.and.domain.usecase
 
-import org.sopt.and.data.model.response.GoogleLoginResponse
+import org.sopt.and.domain.model.AuthToken
 import org.sopt.and.domain.repository.AuthTokenRepository
 import javax.inject.Inject
 
 class UpdateUserRefreshTokenUseCase @Inject constructor(
     private val authTokenRepository: AuthTokenRepository,
 ) {
-    suspend operator fun invoke(refreshBody: GoogleLoginResponse) {
+    suspend operator fun invoke(refreshBody: AuthToken) {
         authTokenRepository.saveAccessToken(refreshBody.accessToken)
         authTokenRepository.saveRefreshToken(refreshBody.refreshToken)
     }
