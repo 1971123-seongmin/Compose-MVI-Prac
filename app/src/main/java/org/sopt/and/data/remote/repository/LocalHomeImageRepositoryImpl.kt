baacce8720af99d79564e631dc0e02c7cd3c2b90
seognmin <arcn1345@gmail.com>
2025-01-12 01:09:52 +0900

    [chore/#9] LocalHomeImageDataSource -> LocalHomeImageDataSource 파일 이름 변경

--- app/src/main/java/org/sopt/and/data/remote/repository/LocalHomeImageRepositoryImpl.kt
+++ app/src/main/java/org/sopt/and/data/remote/repository/LocalHomeImageRepositoryImpl.kt
@@ -1,12 +1,12 @@
 package org.sopt.and.data.remote.repository
 
-import org.sopt.and.data.remote.datasource.local.LocalHomeImageDataSource
+import org.sopt.and.data.remote.datasource.local.LoadHomeImageLocalDataSource
 import org.sopt.and.domain.repository.LocalHomeImageRepository
 import org.sopt.and.presentation.data.HomeData
 import javax.inject.Inject
 
 class LocalHomeImageRepositoryImpl @Inject constructor(
-    private val localHomeImageDataSource: LocalHomeImageDataSource
+    private val localHomeImageDataSource: LoadHomeImageLocalDataSource
 ) : LocalHomeImageRepository {
     override suspend fun getHomeData(): Result<HomeData> {
         return localHomeImageDataSource.getHomeData()
